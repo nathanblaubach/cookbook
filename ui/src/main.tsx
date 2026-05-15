9f559bece628895b0fb4132a7080ac5b2df85f06
Nathan Blaubach <nathanblaubach@gmail.com>
2026-05-15 16:38:15 -0500

    Remove unused recipe form code

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { RecipeRepository } from "./repositories/recipe-repository.ts";
 import { AboutPage } from "./pages/AboutPage.tsx";
-import { RecipeFormPage } from "./pages/recipes/RecipeFormPage.tsx";
 import { RecipePage } from "./pages/recipes/RecipePage.tsx";
 import { RecipeSearchPage } from "./pages/recipes/RecipeSearchPage.tsx";
 import { RecipeUseCases } from "./use-cases/recipe-use-cases.ts";
@@ -25,10 +24,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         {
           path: "recipes",
           element: <RecipeSearchPage recipeUseCases={recipeUseCases} />,
-        },
-        {
-          path: "recipes/add",
-          element: <RecipeFormPage recipeRepository={recipeRepository} />,
         },
         {
           path: "recipes/:id",
