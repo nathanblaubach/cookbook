a5b791febdc55ce1a3d495e545403138089b3fe4
Nathan Blaubach <nathanblaubach@gmail.com>
2026-05-21 22:46:08 -0500

    Require 100% test coverage

--- ui/vitest.config.ts
+++ ui/vitest.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
     setupFiles: ["./src/test-setup.ts"],
     coverage: {
       exclude: ["src/**/*.css", "src/**/*.svg", "src/**/*.json"],
+      thresholds: {
+        statements: 100,
+        branches: 100,
+        functions: 100,
+        lines: 100,
+      },
     },
   },
 });
