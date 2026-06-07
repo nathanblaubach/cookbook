df10c900be5b9f5163c0b3141311498702189940
Nathan Blaubach <nathanblaubach@gmail.com>
2026-06-06 19:55:49 -0500

    Transition from eslint-plugin-react to @eslint-react/eslint-plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import pluginReact from "@eslint-react/eslint-plugin";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -26,12 +26,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ...pluginReact.configs.flat.recommended,
-    files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    settings: { react: { version: "detect" } },
-  },
-  {
-    ...pluginReact.configs.flat["jsx-runtime"],
+    ...pluginReact.configs["recommended-typescript"],
     files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"],
   },
 ];
