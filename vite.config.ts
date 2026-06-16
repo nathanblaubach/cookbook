a8304add534af179fceec16439464f1fa5fcf4eb
Nathan Blaubach <nathanblaubach@gmail.com>
2026-06-15 21:51:28 -0500

    Consolidate vite and vitest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    exclude: ["e2e/**", "node_modules/**"],
+    coverage: {
+      exclude: ["src/**/*.css", "src/**/*.svg", "src/**/*.json"],
+      thresholds: {
+        statements: 100,
+        branches: 100,
+        functions: 100,
+        lines: 100,
+      },
+    },
+  },
 });
