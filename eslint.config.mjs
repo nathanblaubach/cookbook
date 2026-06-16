da2ea0b14fd9414c0efa43e84ddb5f614a9be698
Nathan Blaubach <nathanblaubach@gmail.com>
2026-06-15 22:41:49 -0500

    Make eslint ignores import gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,19 +2,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "@eslint-react/eslint-plugin";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "url";
+import path from "path";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  includeIgnoreFile(
+    path.resolve(path.dirname(fileURLToPath(import.meta.url)), ".gitignore"),
+  ),
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  {
-    ignores: [
-      "coverage/**",
-      "dist/**",
-      "node_modules/**",
-      "playwright-report/**",
-      "test-results/**",
-    ],
-  },
   {
     files: ["e2e/**/*.{js,mjs,cjs,ts}"],
     languageOptions: { globals: globals.node },
